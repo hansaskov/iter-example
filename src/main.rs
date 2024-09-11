72bd2f1fca621d78a9505e67ea16d8f6583234dc
Hans Askov <hans@askov.dk>
2024-09-11 23:27:25 +0200

    Complex rust iterator example

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let values = [0.9, 0.8, 1.2, 1.5, 10.3, 0.7, 2.5, 4.1, 0.7, 2.5, 4.1, 0.2, -3.0, 0.6, 1.3, 2.6, 0.9];
+    println!("Runs");
+    let range: Vec<_> = values
+        .iter()
+        .enumerate()
+        .filter(|(_i, &x)| x > 1.0)
+        .map(|(i, _x)| i)
+        .collect::<Vec<usize>>()
+        .chunk_by(|lhs, rhs| rhs - lhs < 3)
+        .filter_map(|slice| match *slice {
+            [first, .., last] => Some((first, last)),
+            _ => None
+        })
+        .collect();
+
+    for (b, e) in &range {
+        println!("{b:>2} {e:>2}");
+    }
+
+    println!("Longest run");
+    let max = range.iter().max_by_key(|(front, back)| back - front);
+    match max {
+        Some((b, e)) => println!("{b:>2} {e:>2}"),
+        None => println!("Empty!"),
+    }
+}
+

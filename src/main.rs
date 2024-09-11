3dd238366a080c9e234531e09d376490db04d6b7
Hans Askov <hans@askov.dk>
2024-09-11 23:36:22 +0200

    cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let values = [0.9, 0.8, 1.2, 1.5, 10.3, 0.7, 2.5, 4.1, 0.7, 2.5, 4.1, 0.2, -3.0, 0.6, 1.3, 2.6, 0.9];
+    let values = [
+        0.9, 0.8, 1.2, 1.5, 10.3, 0.7, 2.5, 4.1, 0.7, 2.5, 4.1, 0.2, -3.0, 0.6, 1.3, 2.6, 0.9,
+    ];
     println!("Runs");
     let range: Vec<_> = values
         .iter()
@@ -10,7 +12,7 @@ fn main() {
         .chunk_by(|lhs, rhs| rhs - lhs < 3)
         .filter_map(|slice| match *slice {
             [first, .., last] => Some((first, last)),
-            _ => None
+            _ => None,
         })
         .collect();
 
